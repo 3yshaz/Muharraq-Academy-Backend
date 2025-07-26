8c39bf7298b59d9a9da0b6c49633acbaa87a0f62
3yshaz <ayshah.alzayed@gmail.com>
2025-07-26 13:25:46 +0300

    horse route done

--- muharraq-backend/routes/authRoutes.js
+++ muharraq-backend/routes/authRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/authController')
-
+const middleware = require('../midlleware/index')
 
 
 router.post('/signup', controller.signingUp)
-
 router.post('/login', controller.loginUser)
+
+router.get('/', controller.getAllUsers)
+router.get('/:id', controller.getUserById)
+
+router.put('/:id', controller.updateUser)
+
+router.delete('/:id', controller.deleteUser)
 
 
 module.exports = router
---
+++ muharraq-backend/routes/horseRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const horseController = requrie('../controllers/horseController')
+const middleware = require('../middleware/index')
+
+
+router.post('/', horseController.registerHorse)
+
+router.get('/', horseController.getAllHorses)
+router.get('/:id', horseController.getHorseById)
+
+router.put('/:id', horseController.updateHorse)
+
+router.delete('/:id', horseController.deleteHorse)
+
+module.exports = router
